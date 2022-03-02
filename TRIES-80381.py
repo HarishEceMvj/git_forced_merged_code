cc935b9a426183cddf1f3936e66da6311cbc4bc1
HarishEceMvj <99786235+HarishEceMvj@users.noreply.github.com>
2022-03-02 16:07:18 +0530

    Updated TRIES-80381.py with arguments parser

--- TRIES-80381.py
+++ TRIES-80381.py
@@ -1,14 +1,30 @@
 import paramiko
+import argparse
 
 print(" 1.Pre-requests for execution of commands required 'git cli' tool to be installed on your machine \
         2. git repository to be cloned to host, where force merge going to check")
 
-host_ip = input("enter the host_ip\n")
-host_username = input("enter the username\n")
-host_password = input("enter the password\n")
+parser = argparse.ArgumentParser()
+parser.add_argument("Host_IP", type=str, default="10.207.146.173", help="Specify Host_IP")
+parser.add_argument("Host_User_Name", type=str, help="Provide Host Username")
+parser.add_argument("Host_User_Password", type=str, help="Provide Host Password")
+parser.add_argument("repo_cloned_path", type=str, help="Provide git repo cloned path")
 
-git_cloned_directory = input("provide git cloned repository path Example. '/home/cyc/cyclone'")
-# with open("output_file_read.txt", "w+") as f:
+'''execute locally un-uncomment below lines'''
+# host_ip = input("enter the host_ip\n")
+# host_username = input("enter the username\n")
+# host_password = input("enter the password\n")
+# git_cloned_directory = input("provide git cloned repository path Example. '/home/cyc/cyclone'")
+
+
+# Parse the arguments to local variable from CLI
+args = parser.parse_args()
+host_ip = args.Host_IP
+host_username = args.Host_User_Name
+host_password = args.Host_User_Password
+git_cloned_directory = args.repo_cloned_path
+
+# Create a SSH object
 ssh1 = paramiko.SSHClient()
 ssh1.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh1.connect(hostname=host_ip, port=22, username=host_username, password=host_password)
@@ -20,7 +36,7 @@ cmd = f"cd {git_cloned_directory} && {git_cmd}"
 # print(cmd)
 
 # Add all PRs to a list
-Pr_recovered = []  
+Pr_recovered = []  # added one PR to a list, which was forced merged for testing purpose
 stdin, stdout, stderr = ssh1.exec_command(cmd)
 
 # appending all PRs to list
